f97b9551e1afd56ec7062e0211684b503437112f
Dawid <dawid@example.com>
2016-06-21 16:04:05 +0200

    dodanie zakładki Eksploracja danych

---
+++ SmallPrograms/Controllers/DataMiningController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmallPrograms.Controllers
+{
+    public class DataMiningController : Controller
+    {
+        // GET: DataMining
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
