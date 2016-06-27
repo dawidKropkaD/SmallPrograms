b0b80223596763468cd66445ee20c396ca1346c3
Dawid <dawid@example.com>
2016-06-27 15:40:01 +0200

    Dodanie przestrzeni nazw (areas)

---
+++ SmallPrograms/Areas/DataMining/Controllers/ProgramsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmallPrograms.Areas.DataMining.Controllers
+{
+    public class ProgramsController : Controller
+    {
+        // GET: DataMining/Programs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult KMeans()
+        {
+            return View();
+        }
+    }
+}
---
+++ SmallPrograms/Areas/DataMining/DataMiningAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SmallPrograms.Areas.DataMining
+{
+    public class DataMiningAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DataMining";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DataMining_default",
+                "DataMining/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
